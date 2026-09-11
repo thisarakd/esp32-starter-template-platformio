4638b77d88bbf224a3ab72894b49123a1a7713a9
Thisara Kavinda <kavinda.tkd@gmail.com>
2026-09-11 23:24:30 +0530

    Refactor main.cpp to simplify setup and loop functions by removing unnecessary serial prints and delays

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,18 @@
 #include <Arduino.h>
 
-// Standard onboard status LED for ESP32 WROOM-32 DevKits
-#define STATUS_LED 2
+#define STATUS_LED 2  // Onboard LED pin.
 
 void setup() {
     Serial.begin(115200);
-    delay(1000);
 
     pinMode(STATUS_LED, OUTPUT);
-    Serial.println();
-    Serial.println(F("===================================="));
-    Serial.println(F("[STATUS] ESP32 WROOM-32 Booted Cleanly"));
-    Serial.printf("[INFO]   Free Heap: %u bytes\n", ESP.getFreeHeap());
-    Serial.printf("[INFO]   Chip Rev : %u\n", ESP.getChipRevision());
-    Serial.println(F("===================================="));
 }
 
 void loop() {
+    // Blink the LED.
     digitalWrite(STATUS_LED, HIGH);
-    Serial.println(F("[HEARTBEAT] Tick - LED ON"));
     delay(1000);
 
     digitalWrite(STATUS_LED, LOW);
-    Serial.println(F("[HEARTBEAT] Tock - LED OFF"));
     delay(1000);
 }
